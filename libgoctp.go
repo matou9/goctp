38afcef601118b44473ee10eebcdd8ead5f987ba
david_feng <385297@qq.com>
2025-03-02 21:57:25 +0800

    Update libgoctp.go

--- libgoctp.go
+++ libgoctp.go
@@ -2,7 +2,9 @@
 package goctp
 
 /*
-#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.6.5_20210924_api_tradeapi_se_linux64 -Wl,-rpath,${SRCDIR}/api/v6.6.5_20210924_api_tradeapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
+//#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.6.5_20210924_api_tradeapi_se_linux64 -Wl,-rpath,${SRCDIR}/api/v6.6.5_20210924_api_tradeapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
+//#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.6.5_20210924_api_tradeapi_se_linux64
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.6.5_20210924_api_tradeapi_se_linux64 -Wl,-rpath,$ORIGIN -lthostmduserapi_se -lthosttraderapi_se -lstdc++
 #cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.6.5_20210924_api_tradeapi_se_linux64
 
 // windows 不可用，go 部分功能不支持
